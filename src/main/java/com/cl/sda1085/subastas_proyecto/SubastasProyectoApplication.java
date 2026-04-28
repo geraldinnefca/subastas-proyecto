6784da108a1ed0da4697ee69a165e7c9bc4b0ac9
Usuario <geral.gonzalezs@duocuc.cl>
2026-04-27 23:43:47 -0400

    Re-instalando git en subastas

---
+++ src/main/java/com/cl/sda1085/subastas_proyecto/SubastasProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.cl.sda1085.subastas_proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubastasProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubastasProyectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cl/sda1085/subastas_proyecto/SubastasProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cl.sda1085.subastas_proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubastasProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
