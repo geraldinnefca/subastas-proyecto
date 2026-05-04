23bc6dda028f41bfb0e148da256e43f3254b4396
Gonza-2103 <ghormazaba.d@gmail.com>
2026-05-04 12:22:16 -0400

    CRUDs personalizados

--- src/main/java/cl/sda1085/subastas/repository/SubastaRepository.java
+++ src/main/java/cl/sda1085/subastas/repository/SubastaRepository.java
@@ -3,6 +3,31 @@ package cl.sda1085.subastas.repository;
 import cl.sda1085.subastas.model.Subasta;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface SubastaRepository extends JpaRepository<Subasta, Long> {
+
+    //Buscar subastas activas por su estado
+    List<Subasta> findByEstado(String estado);
+
+    //Buscar subastas de un producto específico
+    List<Subasta> findByIdProducto(Long idProducto);
+
+    //Buscar subastas que finalizan antes de una fecha/hora específica
+    List<Subasta> findByFechaFinBefore(LocalDateTime fecha);
+
+    //Busca la subasta activa de un producto específico
+    Optional<Subasta> findByIdProductoAndEstado(Long idProducto, String estado);
+
+    //Verifica si un vendedor ya tiene una subasta activa de un producto específico
+    boolean existsByIdVendedorAndEstado(Long idVendedor, String estado);
+
+    //Encuentra la subasta que terminará más pronto (la más urgente)
+    Optional<Subasta> findTopByEstadoOrderByFechaFinAsc(String estado);
+
+    //Verifica si un producto ya está registrado en alguna subasta, sin importar el estado
+    boolean existsByIdProducto(Long idProducto);
 
 }
--- src/main/java/cl/sda1085/subastas/service/SubastaService.java
+++ src/main/java/cl/sda1085/subastas/service/SubastaService.java
@@ -1,4 +1,22 @@
 package cl.sda1085.subastas.service;
 
+import cl.sda1085.subastas.dto.SubastaResponseDTO;
+import cl.sda1085.subastas.model.Subasta;
+import cl.sda1085.subastas.repository.SubastaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SubastaService {
+
+    private final SubastaRepository subastaRepository;
+
+    //Método de apoyo para encapsulamiento de datos
+    private SubastaResponseDTO mapToResponseDTO(Subasta subasta){
+        return new SubastaResponseDTO(
+                subasta.getId(),
+                sub
+        );
+    }
 }
